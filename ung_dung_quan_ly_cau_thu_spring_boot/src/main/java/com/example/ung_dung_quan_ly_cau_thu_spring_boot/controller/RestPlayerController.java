2c737cab1d1d42446d49b20700530cb460c9dfcb
letam2207 <letam.x2004@gamil.com>
2025-11-12 16:52:26 +0700

    up bt

--- ung_dung_quan_ly_cau_thu_spring_boot/src/main/java/com/example/ung_dung_quan_ly_cau_thu_spring_boot/controller/RestPlayerController.java
+++ ung_dung_quan_ly_cau_thu_spring_boot/src/main/java/com/example/ung_dung_quan_ly_cau_thu_spring_boot/controller/RestPlayerController.java
@@ -10,9 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.DuplicateFormatFlagsException;
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("v1/api/players")
 public class RestPlayerController {
